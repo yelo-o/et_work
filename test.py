8a9d211e0e9d3f96f608ef4a28f70d4f33bd9a87
flyordig <flyordig@gmail.com>
2023-03-10 18:03:10 +0900

    다시한번 테스트 커밋

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ from openpyxl import load_workbook
 
 excel_app = xlwings.App(visible=False)
 excel_book = excel_app.books.open('KT&G.xlsx')
-excel_book.save()
+excel_book.save('KT&G_refined.xlsx')
 excel_book.close()
 excel_app.quit()
 
 wb = load_workbook(filename='KT&G.xlsx', data_only=True)
 
-wb.save('KT&G_refined.xlsx')
+wb.save('KT&G_refined2.xlsx')
